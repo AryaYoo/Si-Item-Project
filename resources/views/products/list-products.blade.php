5db91156640ae18a718f6378a879c71316823cb3
AryaYohanes <aarya5558@gmail.com>
2023-07-14 22:54:41 +0700

    list product

---
+++ resources/views/products/list-products.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.layout-1')
+@section('content')
+<div class="row mb-3 mt-4 text-center">
+    <div class="col-md themed-grid-col">
+        <div class="row pt-1 pb-1" style="background-color: #213655;">
+            <div class="col-md-1 thermed-grid-col"></div>
+            <div class="col-md-2 thermed-grid-col text-light border-end">PHOTO</div>
+            <div class="col-md-2 thermed-grid-col text-light border-end">JUDUL</div>
+            <div class="col-md-2 thermed-grid-col text-light border-end">KETERANGAN</div>
+            <div class="col-md-2 thermed-grid-col text-light border-end">HARGA</div>
+            <div class="col-md-2 thermed-grid-col text-light">ACTION</div>
+            <div class="col-md-1 thermed-grid-col"></div>
+        </div>
+    </div>
+</div>
+@endsection
