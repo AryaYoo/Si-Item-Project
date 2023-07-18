8621ab68442c80b8b96897e45b78e21a7294ba03
aidarifdatul <aidahasna42@gmail.com>
2023-07-18 07:03:57 +0700

    halaman admin

---
+++ resources/views/layouts/main/headeradmin.blade.php
@@ -0,0 +1,14 @@
+<header class="py-2 p-1 border-bottom" style="background-color: #213655;">
+    <div class="container">
+        <div class="container-fluid d-grid gap-3 align-items-center justify-content-between"
+            style="grid-template-columns: 1fr 2fr;">
+            <a href="{{ url('/') }}"
+                class="d-flex align-items-center col-lg-4 mb-2 mb-lg-0 link-body-emphasis text-decoration-none">
+                <img src="{{Vite::asset('resources/images/Logo-SiBarang.png')}}" class="bi me-2" width="70" height="70">
+            </a>
+            <div class="text-end">
+                    <a style="color:white;">ADMIN 1</a>
+            </div>
+        </div>
+    </div>
+</header>
