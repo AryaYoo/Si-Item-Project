63b2bffd7e89b18d040bf6ff290a07d424ba6ba4
AryaYoo <105471308+AryaYoo@users.noreply.github.com>
2023-07-18 08:57:54 +0700

    Revert "login"

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,63 +1,66 @@
 @extends('layouts.layout-2')
 
 @section('title')
-    User Profile
+User Profile
 @endsection
 
 @section('content')
-    <div class="container mt-4">
-        {{-- <div class="main-body"> --}}
-        <div class="row gutters-sm">
-            <div class="col-4 mb-3">
-                <div class="card">
-                    <div class="card-body">
-                        <div class="d-flex flex-column align-items-center text-center">
-                            <img src="#" alt="#FotoUser" class="circle" style="width: 150px; height: 150px;">
-                        </div>
+
+<div class="container mt-4">
+    {{-- <div class="main-body"> --}}
+    <div class="row gutters-sm">
+        <div class="col-4 mb-3">
+            <div class="card">
+                <div class="card-body">
+                    <div class="d-flex flex-column align-items-center text-center">
+                        <img src="#" alt="#FotoUser"
+                            class="circle" style="width: 150px; height: 150px;">
                     </div>
                 </div>
             </div>
-            <div class="col-8">
-                <div class="card mb-3">
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-sm-3">
-                                <h6 class="mb-0">Username</h6>
-                            </div>
-                            <div class="col-sm-9 text-secondary">
-                                <p class="text-muted mb-0">#Nama</p>
-                            </div>
+        </div>
+        <div class="col-8">
+            <div class="card mb-3">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-sm-3">
+                            <h6 class="mb-0">Username</h6>
                         </div>
-                        <hr>
-                        <div class="row">
-                            <div class="col-sm-3">
-                                <h6 class="mb-0">Email</h6>
-                            </div>
-                            <div class="col-sm-9 text-secondary">
-                                <p class="text-muted mb-0">#Email</p>
-                            </div>
+                        <div class="col-sm-9 text-secondary">
+                            <p class="text-muted mb-0">#Nama</p>
                         </div>
-                        <hr>
-                        <div class="row">
-                            <div class="col-sm-3">
-                                <h6 class="mb-0">Kota / Kabupaten</h6>
-                            </div>
-                            <div class="col-sm-9 text-secondary">
-                                <p class="text-muted mb-0">#No Telp </p>
-                            </div>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col-sm-3">
+                            <h6 class="mb-0">Email</h6>
                         </div>
-                        <hr>
-                        <div class="row">
-                            <div class="col-sm-3">
-                                <h6 class="mb-0">Alamat Lengkap</h6>
-                            </div>
-                            <div class="col-sm-9 text-secondary">
-                                <p class="text-muted mb-0">#Alamat</p>
-                            </div>
+                        <div class="col-sm-9 text-secondary">
+                            <p class="text-muted mb-0">#Email</p>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col-sm-3">
+                            <h6 class="mb-0">Kota / Kabupaten</h6>
+                        </div>
+                        <div class="col-sm-9 text-secondary">
+                            <p class="text-muted mb-0">#No Telp </p>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col-sm-3">
+                            <h6 class="mb-0">Alamat Lengkap</h6>
+                        </div>
+                        <div class="col-sm-9 text-secondary">
+                            <p class="text-muted mb-0">#Alamat</p>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
+
 @endsection
