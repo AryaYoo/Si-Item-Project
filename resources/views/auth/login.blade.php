0669477a0d7c2169bc70c38fcfdc186346a01e33
AryaYoo <105471308+AryaYoo@users.noreply.github.com>
2023-07-23 18:53:09 +0700

    Revert "Fitri"

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,35 +1,21 @@
 @extends('layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
-    @vite('resources/sass/app.scss')
-</head>
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Login') }}</div>
 
-<body>
+                <div class="card-body">
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
 
-    <body style="background-color:antiquewhite">
+                        <div class="row mb-3">
+                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
 
-        <div class="container-sm my-3 py-5" style="background-color:#213655">
-            <div class="row justify-content-center">
-                <div class="p-5 my- rounded-3 col-xl-5 border">
-                    <div class="mb-10 text-center mb-5">
-                        <i class=""><img src="Logo-SiBarang.png" style=""><div class="form my-5"></i>
-                    <div class="row mb-15" >
-                        <h4 class="fs-5 fw-bold" style="font-color:#ffff">LOGIN TO SI BARANG </h4>
-                    </div>
-                    <div class="form my-5">
-                        <hr>
-                        <form method="POST" action="{{ route('login') }}">
-                            @csrf
-
-                            <div class="row mb-5 w-100px" style="color:hsl(0, 100%, 99%)">
-                                <input id="email" type="email"
-                                    class="form-control @error('email') is-invalid @enderror" name="email"
-                                    value="{{ old('email') }}" required autocomplete="email"
-                                    placeholder="Masukkan Email Anda" autofocus>
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -37,11 +23,13 @@
                                     </span>
                                 @enderror
                             </div>
+                        </div>
 
-                            <div class="row">
-                                <input id="password" type="password"
-                                    class="form-control @error('password') is-invalid @enderror" name="password"
-                                    required autocomplete="current-password" placeholder="Masukkan Password Kamu">
+                        <div class="row mb-3">
+                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -49,27 +37,37 @@
                                     </span>
                                 @enderror
                             </div>
-                            <hr>
-                            <div class="row pt-4">
-                                <button type="submit" class="btn btn-warning btn-lg mt-0">
+                        </div>
 
-                                    <i class="bi bi-box-arrow-center"></i> {{ __('Login') }}
+                        <div class="row mb-3">
+                            <div class="col-md-6 offset-md-4">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                    <label class="form-check-label" for="remember">
+                                        {{ __('Remember Me') }}
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Login') }}
                                 </button>
-                                  <label class="checkbox pull-left">
-                        <input type="checkbox" value="remember-me">
-                        Remember me
-                    </label>
-                    <a href="#" class="pull-right need-help">Need help? </a><span class="clearfix"></span>
+
+                                @if (Route::has('password.request'))
+                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                        {{ __('Forgot Your Password?') }}
+                                    </a>
+                                @endif
+                            </div>
+                        </div>
                     </form>
                 </div>
-                <a href="#" class="text-center new-account">Create an account </a>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-                @vite('resources/js/app.js')
-    </body>
-
-
-</html>
-
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
